97fc353107f80e1ecd339c838546e89492df84b8
JedwardMook <jedwardmook@yahoo.com>
2022-08-01 10:33:46 -0500

    commit tree 046da8946d177c6165834212dbae244dd1e0dc68

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name:"Ben", last_name:"Briggs", grade:9)
+Student.create!(first_name:"Bill", last_name:"Migs", grade:10)
+Student.create!(first_name:"Jen", last_name:"Higgins", grade:9)
